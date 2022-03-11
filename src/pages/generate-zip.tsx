0b470c97b66ad7641b154f752517300efb244ffb
Jeet <taranjeet0007@gmail.com>
2022-03-11 16:16:15 +0700

    set files

--- src/pages/generate-zip.tsx
+++ src/pages/generate-zip.tsx
@@ -1,20 +1,24 @@
 import { CARTER_ENDPOINT } from 'config/env'
 import { useCallback, useState } from 'react'
 
+export interface IFile {
+  path: string
+  url: string
+}
 export const GenetrateZip = () => {
   // get room id from user using a input tag
   const [roomId, setRoomId] = useState('')
+  const [files, setFiles] = useState<Array<IFile>>([])
 
   // callback function to call api to fetch all the files in roomID
   const getFiles = useCallback(async () => {
-    console.log(process.env)
-
     return fetch(`${CARTER_ENDPOINT}/rooms/${roomId}/download`)
   }, [roomId])
 
   const handleSubmit = () => {
-    getFiles().then((res) => {
-      console.log(res)
+    getFiles().then((res: any) => {
+      const { data } = res
+      setFiles(data)
     })
   }
 
@@ -29,6 +33,15 @@ export const GenetrateZip = () => {
         onChange={(e) => setRoomId(e.target.value)}
       />
       <button onClick={handleSubmit}>Generate</button>
+      <ul>
+        {files &&
+          files.length &&
+          files.map((file) => (
+            <li>
+              <label> {file.path} </label>
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
